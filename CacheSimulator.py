798b458659da08aa41f2b244637473fb76abd7cf
John Harkendorff <jharkendorff@gmail.com>
2020-04-24 14:33:46 -0500

    added request generator

--- CacheSimulator.py
+++ CacheSimulator.py
@@ -1,32 +1,65 @@
 #!/usr/bin/env python
+import random
 
 class Cache:
-	# cache_levls: int, the number of cache levels
-	# cache_blocks: array<int>, the number of cache blocks per level
-	def __init__(self, cache_levels, cache_blocks):
-		if cache_levels != len(cache_blocks):
-			return False
+	def __init__(self):
+		return null
+
+# Generates random requests to send to the cache class				
+class Request_Generator:
+	def __init__(self):
+		self.requests = []
+		self.max_value = 10000
+
+	def generate_requests(self, num_requests):
+		for i in range(num_requests):
+			ran_num = random.random()
 			
-		self.cache_levels = cache_levels
-		self.cache_values = []
-		for i in range(0, cache_levels):
-			cache_block = []
-			for j in range(0, cache_blocks[i]):
-				cache_block.append(-1)
-				
-			self.cache_values.append(cache_block)
+			# 10% chance of using the previous request
+			if ran_num < 0.1:
+				self.requests.append(self.prev_value())
+			# 10% chance of using any of the past 5 requests
+			elif ran_num < 0.2:
+				self.requests.append(self.past_five())
+			# 10% chance of using a value close to the previous request's value
+			elif ran_num < 0.3:
+				self.requests.append(self.temporal())
+			# 70% chance of a completely random value
+			else:
+				self.requests.append(self.random_value())
+		
+		return self.requests
 			
+	def prev_value(self):
+		if len(self.requests) == 0:
+			return self.random_value()
+			
+		return self.requests[len(self.requests) - 1]
 		
+	def past_five(self):
+		if len(self.requests) < 5:
+			return self.random_value()
+			
+		ran_num = random.randint(1, 5)
+		return self.requests[len(self.requests) - ran_num]
 		
-	def get_data(self, data_address):
-		data = -1
-		for i in range(0, cache_values):
-			for j in range(0, cache_values[i]):
-				if 
+	def temporal(self):
+		if len(self.requests) == 0:
+			return self.random_value()
+			
+		ran_num = random.randint(-8, 8)
+		appended_value = self.requests[len(self.requests) - 1] + ran_num
+		if appended_value > self.max_value:
+			appended_value = self.max_value
+		elif appended_value < 0:
+			appended_value = 0
 		
+		return appended_value
+		
+	def random_value(self):
+		return random.randint(0, self.max_value)
 		
 if __name__ == "__main__":
-	TODO()
-	
-def TODO():
-	print "gotta do this"
+	request_generator = Request_Generator()
+	requests = request_generator.generate_requests(10000)
+	print(requests)
